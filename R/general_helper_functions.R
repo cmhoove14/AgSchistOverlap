a608e5c40aef4079369fadf967d47a189cf11ab6
Chris Hoover <cmhoove14@gmail.com>
2019-10-04 15:57:58 -0700

    Add function and dependencies to read in shapefiles as R sf objects from googledrive

--- R/general_helper_functions.R
+++ R/general_helper_functions.R
@@ -78,3 +78,34 @@ load_dta_from_googledrive <- function(id){
 
   return(out)
 }
+
+#'  Load a shapefile contained in a googledrive folder as an R sf object
+#'  
+#` You can find folder ids by going to the sharing settings of a particular folder and 
+#'  copy/pasting the alphanumeric string between "https://drive.google.com/file/d/" and "/view?usp=sharing"
+#'  
+#' @param id googledrive folder id
+#'
+#' @return R sf object from shapefile file in googledrive folder
+#' @export
+ 
+load_sf_from_googledrive <- function(id){
+
+#Get all googledrive ids in the folder containing the shapefile
+  file_info <- drive_ls(as_id(id))
+  file_ids <- file_info$id
+  file_names <- file_info$name
+
+#Create folder to place spatial files in    
+  temp <- tempdir()
+  
+#Get all files in the google drive folder and put them in the temp folder  
+  for(i in 1:length(file_ids)){
+    drive_download(as_id(file_ids[i]), path = paste0(temp, "\\", file_names[i]), overwrite = TRUE)
+  }
+  
+# load as sf object 
+  out <- st_read(temp, layer = strsplit(file_names[which(grepl(".shp", file_names))], ".shp")[[1]])
+  
+  return(out)
+}
